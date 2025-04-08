d7a9f776e7af762503e369ab1373a4ec64db39bc
varunriyer <vs7835@srmist.edu.in>
2025-04-08 11:50:31 +0530

    Add comments to clarify behavior of writeFile and appendFile functions

--- write_file_test.js
+++ write_file_test.js
@@ -4,3 +4,7 @@ fs.writeFile('mynewfile3.txt', 'Hello content!!', function (err) {
     if (err) throw err;
     console.log('Saved!');
 });
+
+
+//Therefore the appendfile() adds the content specified content at the end of the specified file
+//Whereas the the writefile() replaces the specified file and content
