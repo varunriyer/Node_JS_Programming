223820646d76b6ab11521a15c422076338879c3f
varunriyer <vs7835@srmist.edu.in>
2025-04-08 11:14:31 +0530

    Update query string response to test how the text message is being displayed

--- splitting_the_query_string.js
+++ splitting_the_query_string.js
@@ -4,6 +4,6 @@ var url = require('url');
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
+    var txt = q.year + " " + q.month + " --> Take me back pls";
     res.end(txt);
 }).listen(8080);
