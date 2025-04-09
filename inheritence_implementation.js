fa91b2fb2d296ef4e708345a51e110fada313407
varunriyer <vs7835@srmist.edu.in>
2025-04-09 11:25:23 +0530

    Implement inheritance with Character, Enemy, Alien, Bug, and Robot classes in inheritence_implementation.js. Showed the working of parent class inheritence as well as grandparent inheritence

---
+++ inheritence_implementation.js
@@ -0,0 +1,54 @@
+class Character {
+    constructor(speed) {
+        this.speed = speed
+    }
+    move = () => console.log(`I'm moving at the speed of ${this.speed}!`)
+}
+
+class Enemy extends Character {
+    constructor(name, phrase, power, speed) {
+        super(speed)
+        this.name = name
+        this.phrase = phrase
+        this.power = power
+    }
+    sayPhrase = () => console.log(this.phrase)
+    attack = () => console.log(`I'm attacking with a power of ${this.power}!`)
+}
+
+
+class Alien extends Enemy {
+    constructor(name, phrase, power, speed) {
+        super(name, phrase, power, speed)
+        this.species = "alien"
+    }
+    fly = () => console.log("Zzzzzziiiiiinnnnnggggg!!")
+}
+
+class Bug extends Enemy {
+    constructor(name, phrase, power, speed) {
+        super(name, phrase, power, speed)
+        this.species = "bug"
+    }
+    hide = () => console.log("You can't catch me now!")
+}
+
+class Robot extends Enemy {
+    constructor(name, phrase, power, speed) {
+        super(name, phrase, power, speed)
+        this.species = "robot"
+    }
+    transform = () => console.log("Optimus prime!")
+}
+
+
+const alien1 = new Alien("Ali", "I'm Ali the alien!", 10, 50)
+const alien2 = new Alien("Lien", "Run for your lives!", 15, 60)
+const bug1 = new Bug("Buggy", "Your debugger doesn't work with me!", 25, 100)
+const bug2 = new Bug("Erik", "I drink decaf!", 5, 120)
+const robot1 = new Robot("Tito", "I can cook, swim and dance!", 125, 30)
+const robot2 = new Robot("Terminator", "Hasta la vista, baby!", 155, 40)
+
+
+alien1.move();
+robot1.attack();
