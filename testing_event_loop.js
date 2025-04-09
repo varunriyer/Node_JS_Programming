591ee8a7c7db73d678bd6f76f201515f41576bdd
varunriyer <vs7835@srmist.edu.in>
2025-04-09 10:31:04 +0530

    Enhance testing_event_loop.js by testing synchronous and asynchronous logging; add sample.txt

--- testing_event_loop.js
+++ testing_event_loop.js
@@ -1,3 +1,13 @@
+const fs = require('fs');
+
+console.log("Synchronous Code Execution");
 console.log("first");
 console.log("second");
+console.log("third");
+
+console.log("Asynchronous Code Execution");
+console.log("first");
+fs.readFile("sample.txt", () => {
+    console.log("second");
+})
 console.log("third");
