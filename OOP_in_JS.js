a067e2bf674f0da157b22dab0f02abc8fa9556c9
varunriyer <vs7835@srmist.edu.in>
2025-04-09 10:48:20 +0530

    Add OOP_in_JS.js with examples of objects and displayed the object properties and called it's member functions

---
+++ OOP_in_JS.js
@@ -0,0 +1,41 @@
+const alien1 = {
+    name: "Ali",
+    species: "alien",
+    phrase: () => console.log("I'm Ali the alien!"),
+    fly: () => console.log("Zzzzzziiiiiinnnnnggggg!!")
+}
+const alien2 = {
+    name: "Lien",
+    species: "alien",
+    sayPhrase: () => console.log("Run for your lives!"),
+    fly: () => console.log("Zzzzzziiiiiinnnnnggggg!!")
+}
+const bug1 = {
+    name: "Buggy",
+    species: "bug",
+    sayPhrase: () => console.log("Your debugger doesn't work with me!"),
+    hide: () => console.log("You can't catch me now!")
+}
+const bug2 = {
+    name: "Erik",
+    species: "bug",
+    sayPhrase: () => console.log("I drink decaf!"),
+    hide: () => console.log("You can't catch me now!")
+}
+const Robot1 = {
+    name: "Tito",
+    species: "robot",
+    sayPhrase: () => console.log("I can cook, swim and dance!"),
+    transform: () => console.log("Optimus prime!")
+}
+const Robot2 = {
+    name: "Terminator",
+    species: "robot",
+    sayPhrase: () => console.log("Hasta la vista, baby!"),
+    transform: () => console.log("Optimus prime!")
+}
+
+console.log(alien1.name);
+console.log(bug2.species);
+Robot1.sayPhrase();
+Robot2.transform();
