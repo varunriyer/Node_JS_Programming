d111c77756e86e5580fdff70f840293cd667b017
varunriyer <vs7835@srmist.edu.in>
2025-04-09 15:01:31 +0530

    Add example of string to number conversions in JavaScript

---
+++ type_conversions_in_JS.js
@@ -0,0 +1,11 @@
+//Converting Strings to Numbers 
+{
+    let str_num = 3.13;
+    console.log(Number(str_num));
+    console.log(Number(Math.PI));
+    console.log(Number(" "));
+    console.log(Number("Zero"));
+    console.log(Number("99 98"));
+    console.log(Number("Hello"));
+
+}
