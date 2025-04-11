0c1f0be606c5a0404be3a37b2e062e2349662c39
varunriyer <vs7835@srmist.edu.in>
2025-04-11 10:04:57 +0530

    Add factorial function implementation to demonstrate recursion in JS

--- recursion_practice.js
+++ recursion_practice.js
@@ -9,3 +9,12 @@
     }
     countdown(5);
 }
+
+//Factorial of a number - Using Recursion
+{
+    function factorial(n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
+    }
+    console.log(factorial(5));
+}
