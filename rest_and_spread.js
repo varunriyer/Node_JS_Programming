6ecdb38016fc24a4238773cb813bfcf818c4dc37
varunriyer <vs7835@srmist.edu.in>
2025-04-11 14:17:50 +0530

    Add printAll function to log all provided arguments to the console

--- rest_and_spread.js
+++ rest_and_spread.js
@@ -4,3 +4,11 @@
     }
     console.log(sum(1, 2, 5));
 }
+{
+    function printAll(...args) {
+        for (let item of args) {
+            console.log(item);
+        }
+    }
+    printAll(1, 2, "hello", [3, 4, 5], { a: 1, b: 2 });
+}
