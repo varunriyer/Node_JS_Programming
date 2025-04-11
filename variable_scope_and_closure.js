4cbd2d92f2866abdef983fec2ba80f8c8a972373
varunriyer <vs7835@srmist.edu.in>
2025-04-11 16:19:53 +0530

    Add examples demonstrating closures and counter functions in JavaScript

--- variable_scope_and_closure.js
+++ variable_scope_and_closure.js
@@ -33,3 +33,36 @@ function testScopes() {
 }
 
 testScopes();
+
+//Closures
+function outer() {
+    let name = "Varun";
+
+    function inner() {
+        console.log("Hello" + " " + name);
+    }
+    return inner;
+}
+const greetVarun = outer();
+greetVarun();
+
+function counter() {
+    let count = 0;
+
+    return function () {
+        count++;
+        console.log(count);
+    };
+}
+
+const increment = counter();
+increment(); // 1
+increment(); // 2
+increment(); // 3
+
+const A = counter();
+A();
+A();
+
+const B = counter();
+B();
