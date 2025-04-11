6c56de551bdcebc73c1fbec07efccc5265272793
varunriyer <vs7835@srmist.edu.in>
2025-04-11 11:14:31 +0530

    Add printReverse function to print numbers in reverse order using recursion

--- recursion_practice.js
+++ recursion_practice.js
@@ -92,3 +92,13 @@
     }
     print_fibonacci(6);
 }
+
+{
+    console.log("Printing Reverse:");
+    function printReverse(n) {
+        if (n <= 0) return;
+        console.log(n);
+        printReverse(n - 1);
+    }
+    printReverse(5);
+}
