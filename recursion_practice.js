8c60516747ce3587d87b35e3b2a8db77c440babd
varunriyer <vs7835@srmist.edu.in>
2025-04-11 12:18:52 +0530

    Add fibMemo function for calculating Fibonacci numbers using memoization

--- recursion_practice.js
+++ recursion_practice.js
@@ -193,3 +193,16 @@
     }
     factorialUsingStack(5);
 }
+
+{
+    function fibMemo(n, memo = {}) {
+        if (n <= 1) return n;
+        if (memo[n]) return memo[n];
+
+        memo[n] = fibMemo(n - 1, memo) + fibMemo(n - 2, memo);
+        return memo[n];
+    }
+
+    console.log(fibMemo(10));
+
+}
