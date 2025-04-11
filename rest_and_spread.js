a8d998f6e85c448f33deee20ee185e3c9fd6e599
varunriyer <vs7835@srmist.edu.in>
2025-04-11 14:20:32 +0530

    Add shallow copy example using spread operator in rest_and_spread.js

--- rest_and_spread.js
+++ rest_and_spread.js
@@ -12,3 +12,8 @@
     }
     printAll(1, 2, "hello", [3, 4, 5], { a: 1, b: 2 });
 }
+{
+    const arr1 = [1, 2, 3];
+    const arr2 = [...arr1]; // shallow copy using spread operator
+    console.log(arr2);
+}
