fc47420114bc5f5c9805d45fbcf8648c88dbf25c
varunriyer <vs7835@srmist.edu.in>
2025-04-15 14:39:37 +0530

    Add simulation of setTimeout and Promise to demonstrate execution order

--- NodeJS_Revision.js
+++ NodeJS_Revision.js
@@ -8,3 +8,13 @@
 
     emitter.emit('login', 'Varun');
 }
+
+//Simulating SetTimeout & Promise to check which will evaluate first 
+
+console.log("Start"); //1
+
+setTimeout(() => console.log("Timeout"), 0); //4
+
+Promise.resolve().then(() => console.log("Promise")); //3
+
+console.log("End"); //2
