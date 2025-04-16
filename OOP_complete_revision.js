3768a4e249eb49f3983dd4a56fa0485107751d78
varunriyer <vs7835@srmist.edu.in>
2025-04-16 10:40:54 +0530

    Add car object implementation with properties and displayInfo method

--- OOP_complete_revision.js
+++ OOP_complete_revision.js
@@ -9,3 +9,16 @@
     };
     Person.greet();
 }
+
+// Q1) Create an object car with properties: brand, model, year. Add a method displayInfo that logs the car info.
+{
+    const car = {
+        brand: "BMW",
+        model: "X3",
+        year: 2003,
+        displayInfo: function () {
+            console.log(`This is a ${this.brand}-${this.model} from ${this.year}`);
+        }
+    };
+    car.displayInfo();
+}
