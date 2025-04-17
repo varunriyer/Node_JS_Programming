40c5652fe648b5a477e7709da21d72a941b091f0
varunriyer <vs7835@srmist.edu.in>
2025-04-17 12:13:10 +0530

    Refactor code structure for clarity and organization; move variable declarations and function definitions for better readability.

--- js_from_docs.js
+++ js_from_docs.js
@@ -7,12 +7,28 @@
     const { bar } = foo;
     console.log(bar);
 }
-{
-    console.log(x === undefined);
-    var x = 3;
 
-    (function () {
-        console.log(x);
-        var x = "local value";
-    })();
+console.log(x === undefined);
+var x = 3;
+
+(function () {
+    console.log(x);
+    var x = "local value";
+})();
+
+{
+    const sales = "Toyota";
+
+    function carTypes(name) {
+        return name === "Honda" ? name : `Sorry, we don't sell ${name}.`;
+    }
+
+    const car = { myCar: "Saturn", getCar: carTypes("Honda"), special: sales };
+
+    const car2 = { myCar: "Saturn", getCar: carTypes("Benz"), special: sales };
+
+    console.log(car.myCar);
+    console.log(car.getCar);
+    console.log(car2.getCar);
+    console.log(car.special);
 }
