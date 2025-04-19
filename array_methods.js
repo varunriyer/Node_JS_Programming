966cf77e43e9549043e08d1ab7557328b431ca47
varunriyer <vs7835@srmist.edu.in>
2025-04-19 14:27:29 +0530

    Add demonstration of using concat with array-like objects

--- array_methods.js
+++ array_methods.js
@@ -43,3 +43,12 @@
 
     console.log(arr6);
 }
+
+
+// Using concat with array-like objects (using Symbol.isConcatSpreadable)
+{
+    const obj1 = { 0: 1, 1: 2, 2: 3, length: 3 };
+    const obj2 = { 0: 1, 1: 2, 2: 3, length: 3, [Symbol.isConcatSpreadable]: true };
+
+    console.log([0].concat(obj1, obj2));
+}
